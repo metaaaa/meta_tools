ad8f48a00469b12f7dc347b708dadb4cb3d1130c
metaaaa <metaconcerto@gmail.com>
2021-10-02 16:41:24 +0900

    folder_initializer

---
+++ Assets/meta_folder_initializer/MetaaaFolderInitializer.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+using System.IO;
+
+public class MetaaaFolderInitializer : EditorWindow
+{
+    private static string baseFolderName = "__WorkSpace";
+
+    [MenuItem("metaaa/folder_initialize")]
+    static void CreateFolder()
+    {
+
+        if (AssetDatabase.IsValidFolder("Assets/"+baseFolderName))
+        {
+            return;
+        }
+
+        FolderName("Animations");
+        FolderName("AnimatorControllers");
+        FolderName("Editors");
+        FolderName("Textures");
+        FolderName("Materials");
+        FolderName("Prefabs");
+        FolderName("Resources");
+        FolderName("RenderTextures");
+        FolderName("Scripts");
+        FolderName("Scenes");
+        FolderName("Shaders");
+
+    }
+
+
+    static void FolderName(string folderName)
+    {
+        string path = "Assets/"+baseFolderName+"/" + folderName;
+
+        if (!Directory.Exists(path))
+        {
+            Directory.CreateDirectory(path);
+        }
+
+        AssetDatabase.ImportAsset(path);
+    }
+}
