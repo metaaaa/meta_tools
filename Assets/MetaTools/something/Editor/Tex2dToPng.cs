e6d585ff277487d3dc692a6261191939295ebdfb
metaaaa <metaconcerto@gmail.com>
2021-10-10 15:49:39 +0900

    add tex2dtoexr

--- Assets/MetaTools/something/Editor/Tex2dToPng.cs
+++ Assets/MetaTools/something/Editor/Tex2dToPng.cs
@@ -13,5 +13,20 @@ public class Tex2dToPng
             return;
         var pngData = tex.EncodeToPNG();
         System.IO.File.WriteAllBytes(Application.dataPath + "/tex.png", pngData);
+        Object.DestroyImmediate(tex);
+    }
+}
+
+public class Tex2dToExr
+{
+    [MenuItem("metaaa/something/tex2exr")]
+    public static void SaveSelection()
+    {
+        var tex = (Texture2D)Selection.activeObject;
+        if (tex == null)
+            return;
+        var exrData = tex.EncodeToEXR(Texture2D.EXRFlags.CompressZIP);
+        System.IO.File.WriteAllBytes(Application.dataPath + "/tex.exr", exrData);
+        Object.DestroyImmediate(tex);
     }
 }
